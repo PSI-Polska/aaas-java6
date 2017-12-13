c482508d3a3d5e944a9c37d8db23577622a0b6c0
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2017-12-13 17:53:37 +0100

    impl: Prepare fixed facade

---
+++ projects/aaas-gateway/src/main/kotlin/pl/psi/aaas/CdiFacade.kt
@@ -0,0 +1,36 @@
+package pl.psi.aaas
+
+import pl.psi.aaas.engine.REngineConfiguration
+import pl.psi.aaas.engine.RServeEngine
+import pl.psi.aaas.usecase.*
+import javax.inject.Inject
+
+
+interface Facade {
+    fun callScript(calcDef: CalculationDefinition)
+}
+
+
+/**
+ * Calculation endpoint.
+ */
+class CdiFacade : Facade {
+    @Inject
+    lateinit var exec: ScriptExecution
+
+    override fun callScript(calcDef: CalculationDefinition) {
+        exec.call(calcDef)
+    }
+}
+
+object FixedFacade : Facade {
+    val engine: Engine = RServeEngine(REngineConfiguration("", 1))
+    val synchronizer: ScriptSynchronizer = NoSynchronizationSynchronizer()
+    val tsRepository: TimeSeriesRepository = MockTimeSeriesRepository()
+
+    override fun callScript(calcDef: CalculationDefinition) {
+        val scriptExecution = JustScriptExecution(engine = engine, synchronizer = synchronizer, tsRepository = tsRepository)
+
+        scriptExecution.call(calcDef)
+    }
+}
