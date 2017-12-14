778b5d0c7223ef8c230325217b371bd3e779cc55
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2017-12-14 14:28:10 +0100

    impl: Add logging to Engine

--- projects/aaas-gateway/src/main/kotlin/pl/psi/aaas/engine/REngine.kt
+++ projects/aaas-gateway/src/main/kotlin/pl/psi/aaas/engine/REngine.kt
@@ -7,6 +7,7 @@ import org.rosuda.REngine.Rserve.RConnection
 import pl.psi.aaas.usecase.CalculationDefinition
 import pl.psi.aaas.usecase.Engine
 import pl.psi.aaas.usecase.MappedTS
+import pl.psi.aaas.usecase.Symbol
 
 
 internal class RServeEngine(private val configuration: REngineConfiguration) : Engine {
@@ -19,20 +20,31 @@ internal class RServeEngine(private val configuration: REngineConfiguration) : E
         sendValues(tsValues, conn)
 //        setAdditionalParameters()
         val resultDf = execute(conn).asList()
-        return mapDfToTS(resultDf, calcDef)
+        logDataFrame(resultDf)
+        return mapDataFrameToTS(resultDf, calcDef)
     }
 
-    private fun mapDfToTS(resultDf: RList, calcDef: CalculationDefinition): MappedTS {
-        val (results, noResults) = calcDef.timeSeriesIdsOut
+    private fun logDataFrame(resultDf: RList) {
+        val dfColumns = resultDf.keys().joinToString()
+        log.debug("""Result DF columns: $dfColumns""")
+    }
+
+    private fun mapDataFrameToTS(resultDf: RList, calcDef: CalculationDefinition): MappedTS {
+        val (results, missingResults) = calcDef.timeSeriesIdsOut
                 .map { it.key to resultDf[it.key] as REXP? }
                 .partition { it.second != null }
 
-        return if (noResults.isNotEmpty()) {
-            log.error("""Definition: ${calcDef.calculationScriptPath} did not return required symbols: $noResults""")
+        return if (missingResults.isNotEmpty()) {
+            logMissingResults(missingResults, calcDef)
             // TODO 14.12.2017 kskitek: throw exception mby?!
             emptyList()
         } else results.map { it.first to it.second!!.asDoubles() }
 
+    }
+
+    private fun logMissingResults(missingResults: List<Pair<Symbol, REXP?>>, calcDef: CalculationDefinition) {
+        val missingSymbols = missingResults.map { it.first }.joinToString()
+        log.error("""Definition: ${calcDef.calculationScriptPath} did not return required symbols: $missingSymbols""")
     }
 
     private fun execute(conn: RConnection): REXP {
@@ -42,8 +54,9 @@ internal class RServeEngine(private val configuration: REngineConfiguration) : E
     }
 
     private fun sendValues(tsValues: MappedTS, conn: RConnection) {
-        tsValues.forEach { conn.assign(it.first, it.second) }
         val allVectors = tsValues.map { it.first }.joinToString()
+        log.debug("""Sending values $allVectors""")
+        tsValues.forEach { conn.assign(it.first, it.second) }
         conn.voidEval("""dfIn <- data.frame($allVectors)""")
     }
 
@@ -55,4 +68,4 @@ internal class RServeEngine(private val configuration: REngineConfiguration) : E
     private fun getConnection(): RConnection = RConnection(configuration.address, configuration.port)
 }
 
-data class REngineConfiguration(val address: String, val port: Int)
+internal data class REngineConfiguration(val address: String, val port: Int)
