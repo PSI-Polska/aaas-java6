58fe5654ebb16b05602b6b4d5db43c311a6336c8
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2017-12-14 15:55:50 +0100

    infra: project cleanup

--- projects/aaas-gateway/src/main/kotlin/pl/psi/aaas/Facade.kt
+++ projects/aaas-gateway/src/main/kotlin/pl/psi/aaas/Facade.kt
@@ -1,24 +1,8 @@
 package pl.psi.aaas
 
-import pl.psi.aaas.engine.REngineConfiguration
-import pl.psi.aaas.engine.RServeEngine
-import pl.psi.aaas.usecase.*
-import javax.inject.Inject
+import pl.psi.aaas.usecase.CalculationDefinition
 
 
 interface Facade {
     fun callScript(calcDef: CalculationDefinition)
-}
-
-
-/**
- * Calculation endpoint.
- */
-class CdiFacade : Facade {
-    @Inject
-    lateinit var exec: ScriptExecution
-
-    override fun callScript(calcDef: CalculationDefinition) {
-        exec.call(calcDef)
-    }
 }
