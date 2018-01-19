f05641b7bd3d98299bba27e181c53f998069b619
rbachorz <rbachorz@psi.pl>
2018-01-19 13:29:30 +0100

    Training/forecasting improvements...

--- projects/engine-r/src/R/standard/forecastSVM.R
+++ projects/engine-r/src/R/standard/forecastSVM.R
@@ -2,7 +2,6 @@
 # Objective : TODO
 # Created by: kskitek
 # Created on: 2017-08-31
-
 library(timeDate)
 library(dplyr)
 library(TTR)
@@ -10,7 +9,7 @@ library(caret)
 library(e1071)
 library(randomForest)
 
-prepareDataForForecast <- function(dfParameters){
+prepareDataForForecast <- function(dfData, dfParameters){
     forecastBeg <- as.character(dfParameters %>%
         filter(name == "forecastBeg") %>%
         summarise(value))
@@ -20,40 +19,53 @@ prepareDataForForecast <- function(dfParameters){
     resolution <- as.character(dfParameters %>%
         filter(name == "resolution") %>%
         summarise(value))
-    seasonLevel <- as.integer(dfParameters %>%
-        filter(name == "seasonLevel") %>%
-        summarise(value))
 
-    print("Preparing forecast vector")
-    dfTimeseries <- as.data.frame(seq(from = as.POSIXct(forecastBeg, format = "%Y-%m-%dT%H:%M"), to = as.POSIXct(forecastEnd, format = "%Y-%m-%dT%H:%M"), by = resolution))
-    colnames(dfTimeseries) <- c("DateTime")
-    dfTimeseries$seasonLevels <- factor(x = rep(seasonLevel, nrow(dfTimeseries)), levels = c("1", "2", "3"))
-    return(dfTimeseries)
+
+    print("Preparing vector of predictors")
+    dfPredictors <- as.data.frame(seq(from = as.POSIXct(forecastBeg, format = "%Y-%m-%dT%H:%M"), to = as.POSIXct(forecastEnd, format = "%Y-%m-%dT%H:%M") - 1, by = resolution))
+    #print(nrow(dfPredictors))
+    #print(dfPredictors)
+    #print(nrow(dfData))
+    colnames(dfPredictors) <- c("DateTime")
+    dfPredictors$Temperature <- dfData$Temperature
+    dfPredictors$WorkingDay <- as.factor(dfData$WorkingDay)
+    
+    #print(dfPredictors)
+    return(dfPredictors)
 }
 
 run <- function(dfData, dfParameters){
-    filePath <- as.character(dfParameters %>%
-        filter(name == "pathModelIn") %>%
-        summarise(value))
-
-    dfData <- prepareDataForForecast(dfParameters)
-
-    print("Prepare data")
-    # create day of week feature
+    print("Entering forecast")
+    filePath <- as.character(dfParameters %>% filter(name == "pathModelIn") %>% summarise(value))
+    print("Entering prepareDataForForecast")
+    dfData <- prepareDataForForecast(dfData, dfParameters)
+    print("Left prepareDataForForecast")
+    
+    #calendar preciction creation
+    str(dfData)
     dfData$dayOfWeek <- as.factor(dayOfWeek(timeDate(dfData$DateTime)))
-    # create hour feature
     dfData$hour <- as.factor(format(dfData$DateTime, "%H"))
-
-    dataset <- data.frame(predict(dummyVars(~ dayOfWeek + hour + seasonLevels + DateTime, data = dfData), newdata = dfData))
-    dataset$DateTime <- as.POSIXct(dataset$DateTime, origin = "1970-01-01", tz = "GMT")
-
+    dfData$month <- as.factor(format(dfData$DateTime, "%m"))
+    print("calendar predictors: done")
+    str(dfData)
+    
+    #one-hot encoding
+    #definition of the size of space
+    levels(dfData$WorkingDay) <- c("0", "1")
+    levels(dfData$dayOfWeek) <- c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+    levels(dfData$hour) <- c("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23")
+    levels(dfData$month) <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+    
+    DVobject <- dummyVars(~ dayOfWeek + hour + month + Temperature + WorkingDay, data = dfData)
+    featuresResponse.DF <- as.data.table(predict(DVobject, newdata = dfData))
+    print("one-hot encoding: done")
+    
     print("Read model")
     model <- readRDS(filePath)
-    NdataSetCols <- ncol(dataset)
+    #NdataSetCols <- ncol(dataset)
 
     print("Predict")
-    Prediction <- predict(model, dataset[, 1 : (NdataSetCols - 1)])
-
+    Prediction <- predict(model, featuresResponse.DF)
     return(data.frame(Prediction))
 }
 
