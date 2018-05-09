479d6dd25346df4adeecab0ddb01005466d58fb3
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2018-05-09 07:50:44 +0200

    impl: implement TSDataFrame
    looks like it is working now. No tests tho

--- projects/api/src/main/kotlin/pl/psi/aaas/usecase/timeseries/TSCalculationExecution.kt
+++ projects/api/src/main/kotlin/pl/psi/aaas/usecase/timeseries/TSCalculationExecution.kt
@@ -3,6 +3,7 @@ package pl.psi.aaas.usecase.timeseries
 import pl.psi.aaas.Engine
 import pl.psi.aaas.usecase.CalculationException
 import pl.psi.aaas.usecase.CalculationExecution
+import java.time.Duration
 
 
 /**
@@ -15,19 +16,22 @@ class TSCalculationExecution(val tsRepository: TSRepository,
     // TODO 05.05.2018 kskitek: bring synchronizer back
 
     override fun call(calcDef: TSCalculationDefinition) {
-        val inTs = calcDef.timeSeriesIdsIn.map { it.key to tsRepository.read(prepQuery(it.value, calcDef)) }.toMap()
+        val resolution = Duration.ofHours(1)
+        val inTs = calcDef.timeSeriesIdsIn.map { it.key to tsRepository.read(prepQuery(it.value, calcDef)) }
+                .map { it.first to Triple(calcDef.begin, resolution, it.second) }.toMap()
 
         val resultDF = engine.call(TSCalcDefWithValues(calcDef, TSDataFrame(inTs)))
 
-        val mappedResult = resultDF?.toMappedTS() ?: emptyMap()
-        mappedResult.map { symbolToTsId(calcDef, it) to it.value }
+        val mappedResult = resultDF?.toMap() ?: emptyMap()
+        mappedResult.map { symbolToOutTsId(calcDef, it) to it.value }
                 .forEach { tsRepository.save(prepQuery(it.first, calcDef), it.second) }
     }
 
     private fun prepQuery(id: Long, calcDef: TSCalculationDefinition) =
             TSQuery(id, calcDef.begin, calcDef.end)
 
-    private fun symbolToTsId(calcDef: TSCalculationDefinition, it: Map.Entry<Symbol, TS>) =
+    private fun symbolToOutTsId(calcDef: TSCalculationDefinition, it: Map.Entry<Symbol, TS>) =
             calcDef.timeSeriesIdsOut[it.key]
                     ?: throw CalculationException("""Unable to match result time series ${it.key} with definition ${calcDef.timeSeriesIdsOut.keys}.""")
 }
+
--- projects/sample/src/main/kotlin/pl/psi/aaas/sample/MockTimeSeriesRepository.kt
+++ projects/sample/src/main/kotlin/pl/psi/aaas/sample/MockTimeSeriesRepository.kt
@@ -1,7 +1,6 @@
 package pl.psi.aaas.sample
 
 import org.slf4j.LoggerFactory
-import pl.psi.aaas.usecase.Column
 import pl.psi.aaas.usecase.timeseries.TS
 import pl.psi.aaas.usecase.timeseries.TSQuery
 import pl.psi.aaas.usecase.timeseries.TSRepository
@@ -11,15 +10,15 @@ internal class MockTimeSeriesRepository : TSRepository {
 
     private val log = LoggerFactory.getLogger(MockTimeSeriesRepository::class.java)
 
-    override fun read(query: TSQuery): TS {
-        val hours = Duration.between(query.begin, query.end).toHours()
-        return Triple(query.begin, Duration.ofHours(1), (0 until hours).map { it.toDouble() }.toTypedArray() as Column<Double?>)
-    }
+    override fun read(query: TSQuery): TS =
+            with(Duration.between(query.begin, query.end).toHours()) {
+                return (0 until this).map { it.toDouble() }.toTypedArray()
+            }
 
     override fun save(query: TSQuery, values: TS) {
         log.info("SAVING")
         log.info("\ttsId = ${query.tsId}")
-        log.info("\tsize = ${values.third.size}")
+        log.info("\tsize = ${values.size}")
     }
 
 }
