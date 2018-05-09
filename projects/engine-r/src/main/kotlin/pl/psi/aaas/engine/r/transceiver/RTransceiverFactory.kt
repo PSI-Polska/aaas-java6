8ea772a179c442c4937468aa9a460ad973fa5c90
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2018-05-09 19:16:23 +0200

    impl: Implement all send/receive with Transceiver
    WIP: BooleanArrays

--- projects/engine-r/src/main/kotlin/pl/psi/aaas/engine/r/transceiver/RTransceiverFactory.kt
+++ projects/engine-r/src/main/kotlin/pl/psi/aaas/engine/r/transceiver/RTransceiverFactory.kt
@@ -1,5 +1,7 @@
 package pl.psi.aaas.engine.r.transceiver
 
+import org.rosuda.REngine.REXPDouble
+import org.rosuda.REngine.REXPLogical
 import org.rosuda.REngine.REXPString
 import org.rosuda.REngine.Rserve.RConnection
 import pl.psi.aaas.engine.r.RValuesTransceiver
@@ -16,13 +18,32 @@ object RValuesTransceiverFactory {
             if (param.primitive)
                 when (param.clazz) {
                     String::class.java        -> RNativeTransceiver<Parameter<String>, Parameter<String>, D>({ REXPString(it.value) }, conn)
+                    Long::class.java          -> RNativeTransceiver<Parameter<Long>, Parameter<Long>, D>({ REXPDouble(it.value.toDouble()) }, conn)
+                    Double::class.java        -> RNativeTransceiver<Parameter<Double>, Parameter<Double>, D>({ REXPDouble(it.value) }, conn)
+                    Boolean::class.java       -> RNativeTransceiver<Parameter<Boolean>, Parameter<Boolean>, D>({ REXPLogical(it.value) }, conn)
                     ZonedDateTime::class.java -> DateTimeTransceiver<D>(conn) as RValuesTransceiver<*, *, *>
                     else                      -> throw CalculationException("Not implemented parameter type ${param.clazz}")
                 } as RValuesTransceiver<Parameter<*>, *, D>
             else
                 when (param.elemClazz!!) {
-                    String::class.java -> RNativeTransceiver<Parameter<Array<String>>, Parameter<Array<String>>, D>({ REXPString(it.value) }, conn) as RValuesTransceiver<Parameter<*>, *, D>
-                    else               -> throw CalculationException("Not implemented array parameter type ${param.elemClazz}")
+                    String::class.java  -> RNativeTransceiver<Parameter<Array<String?>>, Parameter<Array<String?>>, D>({ REXPString(it.value) }, conn) as RValuesTransceiver<Parameter<*>, *, D>
+                    Double::class.java  -> RNativeTransceiver<Parameter<Array<Double?>>, Parameter<Array<Double?>>, D>(
+                            { REXPDouble(it.value.map { it ?: REXPDouble.NA }.toDoubleArray()) }
+                            , conn) as RValuesTransceiver<Parameter<*>, *, D>
+                // TODO 09.05.2018 kskitek: this is MONSTER!!
+                    Boolean::class.java -> RNativeTransceiver<Parameter<Array<Boolean?>>, Parameter<Array<Boolean?>>, D>(
+                            {
+                                REXPLogical(it.value
+                                        .map {
+                                            when (it) {
+                                                null  -> REXPLogical.NA
+                                                true  -> REXPLogical.TRUE
+                                                false -> REXPLogical.FALSE
+                                            }
+                                        }.toByteArray())
+                            }
+                            , conn)
+                    else                -> throw CalculationException("Not implemented array parameter type ${param.elemClazz}")
                 } as RValuesTransceiver<Parameter<*>, *, D>
 //    ArrayTransceiver<D>(conn) as RValuesTransceiver<Parameter<*>, *, D>
 
