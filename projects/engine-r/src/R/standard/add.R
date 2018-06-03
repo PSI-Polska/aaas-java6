7c97d8d4e0c2052031b33056eb38758c133c0670
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2018-06-03 23:31:35 +0200

    impl, WIP: handle all in/out parameters with env

--- projects/engine-r/src/R/standard/add.R
+++ projects/engine-r/src/R/standard/add.R
@@ -3,11 +3,11 @@
 # Created by: kskitek
 # Created on: 2017-08-30
 
-#' Adds two series
+#' Adds two vectors
 #' @param A, B
 #' @return C
-run <- function(dfIn, additionalParameters) {
-    str(dfIn)
-    C = dfIn$A + dfIn$B
-    data.frame(C)
+run <- function() {
+    ls.str(environment())
+    C <<- A + B
+    # alternative way: env$C <- A + B
 }
